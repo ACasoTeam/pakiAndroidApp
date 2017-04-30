8cc19dd064c747dc3e944b2d90ef1da1d27b0c8e
giacomocavallo <giacomocavallo.cg@gmail.com>
2017-05-01 00:09:09 +0200

    metodo per chiamare il navigatore googleMaps, ora funziona meglio

---
+++ app/src/main/java/acasoteam/pakistapp/FeedActivity.java
@@ -0,0 +1,18 @@
+package acasoteam.pakistapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+
+    public void close(View view){
+        finish();
+    }
+}
